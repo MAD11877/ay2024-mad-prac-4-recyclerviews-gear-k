90f0d93f95c8f5b83c7139243318ea7ae8e25c39
gear-k <s10241866@connect.np.edu.sg>
2024-05-15 16:10:32 +0800

    week 4

--- app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
@@ -4,19 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ArrayList<User> userList;
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        // Set up the ImageView with AlertDialog
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,5 +48,27 @@ public class ListActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+
+        // Create a list of 20 random users
+        userList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            userList.add(new User(generateRandomName(), generateRandomDescription(), i, new Random().nextBoolean()));
+        }
+
+        // Set up the RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        userAdapter = new UserAdapter(userList);
+        recyclerView.setAdapter(userAdapter);
+    }
+
+    private String generateRandomName() {
+        String[] names = {"John", "Jane", "Alex", "Chris", "Katie", "Laura", "Mike", "David", "Anna", "Tom"};
+        return names[new Random().nextInt(names.length)];
+    }
+
+    private String generateRandomDescription() {
+        String[] descriptions = {"Lorem ipsum", "Dolor sit amet", "Consectetur adipiscing", "Elit", "Sed do eiusmod"};
+        return descriptions[new Random().nextInt(descriptions.length)];
     }
 }
